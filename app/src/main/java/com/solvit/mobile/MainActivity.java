1888df7c8b21cf4f1055715b1845b0f4e30ca268
acado1986 <bogdan.ioan.nechifor@gmail.com>
2022-11-18 06:26:45 +0100

    metodo para escribir

--- app/src/main/java/com/solvit/mobile/MainActivity.java
+++ app/src/main/java/com/solvit/mobile/MainActivity.java
@@ -2,29 +2,31 @@ package com.solvit.mobile;
 
 import static android.content.ContentValues.TAG;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 
-import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 import java.util.Map;
 
-public class MainActivity extends AppCompatActivity  {
+public class MainActivity extends AppCompatActivity {
 
     private TextView prueba;
-    private Button btnTest;
+    private Button btnReadData;
+    private Button btnWriteData;
+    private Button btnUpdateData;
     private String hola;
 
     private ArrayList<NotificationModel> notifications;
@@ -35,43 +37,45 @@ public class MainActivity extends AppCompatActivity  {
         setContentView(R.layout.activity_main);
 
         prueba = findViewById(R.id.prueba);
-        btnTest = findViewById(R.id.btnTest);
+        btnReadData = findViewById(R.id.btnReadData);
+        btnWriteData = findViewById(R.id.btnWriteData);
+        btnUpdateData = findViewById(R.id.btnUpdateData);
 
         Bundle user = getIntent().getExtras();
 
         prueba.setText(user.getString("username"));
 
-        notifications = new ArrayList<NotificationModel>();
-        FirebaseFirestore db= FirebaseFirestore.getInstance();
-        btnTest.setOnClickListener(view -> {
 
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        btnReadData.setOnClickListener(view -> {
+            notifications = new ArrayList<NotificationModel>();
             // Create a reference to the cities collection
             db.collection("events/it/it_events")
 //                .whereEqualTo("role", "admin")
-                   // .whereEqualTo("completed","done")
+                    // .whereEqualTo("completed","done")
                     .get()
                     .addOnCompleteListener(task -> {
                         if (task.isSuccessful()) {
                             for (QueryDocumentSnapshot document :
                                     task.getResult()) {
-                               // Log.d(TAG, document.getId() + " => " + document.getData());
+                                // Log.d(TAG, document.getId() + " => " + document.getData());
                                 Map<String, Object> data = document.getData();
                                 NotificationModel notification = new NotificationModel();
-                                  notification.setId(document.getId());
-                                  notification.setCompleted(data.get("completed").toString());
-                                  notification.setPcNumber((long)data.get("pc_number"));
-                                  notification.setRole(data.get("role").toString());
-                                  notification.setRoom(data.get("room").toString());
-                                  notification.setBuilding(String.valueOf(data.get("building")));
-                                  notification.setDescription(data.get("description").toString());
-                                  notification.setUser(((DocumentReference)data.get("user")).getPath());
-                                  // get the list of references not working
+                                notification.setId(document.getId());
+                                notification.setCompleted(data.get("completed").toString());
+                                notification.setPc_number((long) data.get("pc_number"));
+                                notification.setRole(data.get("role").toString());
+                                notification.setRoom(data.get("room").toString());
+                                notification.setBuilding(String.valueOf(data.get("building")));
+                                notification.setDescription(data.get("description").toString());
+                                notification.setUser(((DocumentReference) data.get("user")).getPath());
+                                // get the list of references not working
                                 // notification.setFowardTo((List<String>)data.get("fowardTo"));
                                 notifications.add(notification);
                                 // get to notifications activity
                                 Intent intent = new Intent(this, NotificationPanelActivity.class);
                                 Log.d(TAG, String.valueOf(notifications));
-                                intent.putExtra("notifications", (ArrayList<NotificationModel>)notifications);
+                                intent.putExtra("notifications", notifications);
                                 startActivity(intent);
                             }
                         } else {
@@ -126,6 +130,41 @@ public class MainActivity extends AppCompatActivity  {
 //                        }
 //                    });
 
+        });
+
+        btnWriteData.setOnClickListener(view -> {
+            db.collection("events/it/it_events")
+                    .add(new NotificationModel
+                            ("done", 130, "admin", "a20", "goya", "es un test", "user/test", Arrays.asList("user1", "user2")))
+                    .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                        @Override
+                        public void onSuccess(DocumentReference documentReference) {
+                            Log.d(TAG, "DocumentSnapshot written with ID: " + documentReference.getId());
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.w(TAG, "Error adding document", e);
+                        }
+                    });
+        });
+
+        btnUpdateData.setOnClickListener((view) -> {
+            DocumentReference updateRef = db.collection("events/it/it_events").document("A27VhSNe5BpqsDq2nMHb");
+            updateRef
+                    .update("id", updateRef.getId())
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void unused) {
+                            Log.d(TAG, "DocumentSnapshot successfully updated!");
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.w(TAG, "Error updating document", e);
+                        }
+                    });
         });
     }
 }
