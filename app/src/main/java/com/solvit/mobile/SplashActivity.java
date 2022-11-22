c8d5645bfe57901309601b6618cd6bab0617a405
acado1986 <bogdan.ioan.nechifor@gmail.com>
2022-11-22 07:23:59 +0100

    splash screen with a white background

---
+++ app/src/main/java/com/solvit/mobile/SplashActivity.java
@@ -0,0 +1,24 @@
+package com.solvit.mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 1000);
+    }
+}
